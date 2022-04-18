fea4f85e8aa2aa4b9c68c2fd9040925be38890c9
polk90 <92872300+polk90@users.noreply.github.com>
2022-04-18 16:55:20 +0300

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# pylint: disable=C0116,W0613
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Basic example for a bot that uses inline keyboards. For an in-depth explanation, check out
- https://git.io/JOmFw.
-"""
 import logging
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
@@ -19,36 +11,28 @@ logger = logging.getLogger(__name__)
 
 def start(update: Update, context: CallbackContext) -> None:
     """Sends a message with three inline buttons attached."""
-    query = update.callback_query
     keyboard = [
         [
-            InlineKeyboardButton("Начать читать книги", callback_data='1'),
-            InlineKeyboardButton("Зарегистрироваться", callback_data='2'),
+            InlineKeyboardButton("Option 1", callback_data='1'),
+            InlineKeyboardButton("Option 2", callback_data='2'),
         ],
+        [InlineKeyboardButton("Option 3", callback_data='3')],
     ]
+
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Здравствуйте!:', reply_markup=reply_markup)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
 def button(update: Update, context: CallbackContext) -> None:
-    """Parses the CallbackQuery and up  dates the message text."""
+    """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
-    query.edit_message_text("Доступные книги: \nКомната\nОстров")
-    star_read_books()
 
-def star_read_books(update: Update, context: CallbackContext):
-    keyboard = [
-        [
-            InlineKeyboardButton("Комната", callback_data='3'),
-            InlineKeyboardButton("Остров", callback_data='4'),
-        ],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите книгу', reply_markup=reply_markup)
+    query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -64,6 +48,7 @@ def main() -> None:
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('help', help_command))
+
     # Start the Bot
     updater.start_polling()
 
